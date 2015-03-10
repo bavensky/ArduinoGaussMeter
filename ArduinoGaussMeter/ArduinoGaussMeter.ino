3f20caee1b7f79594b0a5e6f04b8d252353764f1
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2015-03-10 15:42:32 +0700

    new

--- ArduinoGaussMeter/ArduinoGaussMeter.ino
+++ ArduinoGaussMeter/ArduinoGaussMeter.ino
@@ -2,25 +2,36 @@
   
   LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
   
-  #define XRANGE  50 
   #define Hall A1
   
-  int x,gss;
-  
+  int gss=0, Value=0;
+  float voltage=0;
+
   void setup(){
     lcd.begin(16, 2);
     Serial.begin(9600);
     lcd.home();
-    lcd.print("hello, world!");
+    lcd.print("Arduino !");
+    lcd.setCursor(0,1);
+    lcd.print("    Gauss Meter");
+    delay(3000);
+    lcd.clear();
     }   
     
   void loop(){
-    int aValue =analogRead(Hall);
-    x = map(aValue, 0, 1024, 0, XRANGE);
-    gss = map(aValue, 102, 922, -640, 640);
+    
+    int Value = analogRead(Hall);
+    gss = map(Value, 102, 922, -640, 640);
+    voltage = Value * (5.0 / 1023.0);
+    
+    lcd.home();
+    lcd.print("Volt  : ");
+    lcd.print(voltage);
+    lcd.print(" V ");
     lcd.setCursor(0,1);
     lcd.print("Gauss : ");
     lcd.print(gss);
-    lcd.print("     ");
+    lcd.print(" G ");
+    delay(200);
   } 
 
